2b2d71694fe8e1348bfcb87595524fb00a9f39fd
Hamada Ali Elsayad <35568087+Hamada-Ali@users.noreply.github.com>
2023-01-20 17:01:59 +0200

    committed

---
+++ app.js
@@ -0,0 +1,42 @@
+// composite deisng pattern 
+
+// note: this code in nodejs 
+
+// composite design pattern helps us dealing with hierarchy tree of complex components
+
+// parent class 
+class ContentBuffer {
+    getSize() { }
+  }
+  
+  // Composite Class has children as filebuffer
+  class DirBuffer extends ContentBuffer {
+    constructor() {
+      super();
+      this.bufferList = [];
+    }
+    getSize() {
+      return this.bufferList.map(buf => buf.getSize()).reduce((a, b) => a + b);
+    }
+    addBuffer(buf) {
+      this.bufferList.push(buf);
+    }
+  }
+  // Leaf class
+  class FileBuffer extends ContentBuffer {
+    setBuffer(buf) {
+      this.buf = buf;
+      return this;
+    }
+    getSize() {
+      return this.buf.length || 0;
+    }
+  }
+  
+  const file1 = new FileBuffer().setBuffer(Buffer.from("hello"));
+  const file2 = new FileBuffer().setBuffer(Buffer.from("hello2"));
+  
+  const compositeObj = new DirBuffer();
+  compositeObj.addBuffer(file1);
+  compositeObj.addBuffer(file2);
+  console.log(compositeObj.getSize());
